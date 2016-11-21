f2d83ae545352bd8d64c68f2e23896ad7c71bff2
Qiao Ximing <qiaoximing@gmail.com>
2016-11-21 23:30:45 +0800

    changed cache format

--- image_retraining/tsne_retrain.py
+++ image_retraining/tsne_retrain.py
@@ -1,4 +1,5 @@
 import os.path
+import Image
 import numpy as np
 import tensorflow as tf
 import retrain
@@ -8,49 +9,46 @@ NUM_CHANNELS = 3
 NUM_LABELS = 3
 BATCH_SIZE = 100
 
-def cache_bottlenecks(sess, cache_file, image_dir, jpeg_data_tensor,
-                      bottleneck_tensor):
+def one_hot(i, n):
+    return [int(j == i) for j in range(n)] 
+
+def cache_bottlenecks(sess, image_dir, jpeg_data_tensor, bottleneck_tensor):
     image_lists = retrain.create_image_lists(
         image_dir, testing_percentage=10, validation_percentage=10)
     cache_lists = {'training':[], 'testing':[], 'validation':[]}
-    for label_name, label_lists in image_lists.items():
+    label_num = len(image_lists.items())
+    for label_index, (label_name, label_lists) in enumerate(image_lists.items()):
         for category in ['training', 'testing', 'validation']:
+            cache_lists[category] = {'images':[], 'bottlenecks':[], 'labels':[]}
             category_list = label_lists[category]
-            for index, unused_base_name in enumerate(category_list):
+            for index, unused_base_name in enumerate(category_list[:10]):
                 image_path = retrain.get_image_path(
                     image_lists, label_name, index, image_dir, category)
-                image_data = retrain.gfile.FastGFile(image_path, 'rb').read()
+                #image_data = retrain.gfile.FastGFile(image_path, 'rb').read()
+                image_data = np.array(Image.open(image_path))
                 bottleneck_values = retrain.run_bottleneck_on_image(
                     sess, image_data, jpeg_data_tensor, bottleneck_tensor)
-                cache_lists[category].append([image_data, bottleneck_values])
-    xxx.save(cache_file, cache_lists)
-
-
-def student_model():
-    jpeg_data = tf.placeholder(tf.string, [None], name='jpeg_input')
-    labels = tf.placeholder(tf.float32, [None, NUM_LABELS], name='labels_input')
-
-
+                cache_lists[category].images.append(image_data)
+                cache_lists[category].bottlenecks.append(bottleneck_values)
+                cache_lists[category].labels.append(one_hot(label_index, label_num))
+    np.save('train.images.npy', cache_lists.training.images)
+    np.save('train.bottlenecks.npy', cache_lists.training.bottlenecks)
+    np.save('train.labels.npy', cache_lists.training.labels)
+    np.save('validation.images.npy', cache_lists.validation.images)
+    np.save('validation.bottlenecks.npy', cache_lists.validation.bottlenecks)
+    np.save('validation.labels.npy', cache_lists.validation.labels)
+    np.save('test.images.npy', cache_lists.testing.images)
+    np.save('test.bottlenecks.npy', cache_lists.testing.bottlenecks)
+    np.save('test.labels.npy', cache_lists.testing.labels)
 
 def main():
-    cache_file = './cache.file'
-    if not os.path.exists(cache_file):
-        retrain.maybe_download_and_extract()
-        graph, bottleneck_tensor, jpeg_data_tensor, resized_image_tesnor = (
-            retrain.create_inception_graph())
-        cache_bottlenecks(sess, cache_file, './images', jpeg_data_tensor,
-                          bottleneck_tensor)
-
     sess = tf.Session()
 
-    # calculate bottleneck values of images and cache them on disk
-
-    # add student network
-    jpeg_data, labels = student_model()
-
-    # start training
-    sess.run(tf.initialize_all_variables())
-    for i in range(1000):
+    retrain.maybe_download_and_extract()
+    graph, bottleneck_tensor, jpeg_data_tensor, resized_image_tesnor = (
+        retrain.create_inception_graph())
+    cache_bottlenecks(sess, './image_dir', jpeg_data_tensor,
+                      bottleneck_tensor)
 
 if __name__ == '__main__':
     main()
